1ab471ac08be62b076d29cc5a7664f370f05b83b
Tom Rossen <rossentj@acm.org>
2009-12-03 17:26:36 -0600

    Entry extractor 2 red-cooked (should put one matched entry on the queue)

--- pipeline/w1913_entry_extractor_spec.rb
+++ pipeline/w1913_entry_extractor_spec.rb
@@ -3,15 +3,34 @@ require 'pipeline/w1913_entry_extractor'
 describe W1913EntryExtractor do
   EOS = '***EOS'
   
-  it "should put EOS on queue when no entries in input" do
-    extractor = W1913EntryExtractor.new('','', EOS)
-    inqueue = Queue.new
-    outqueue = Queue.new
-    t = Thread.new do
-      extractor.extract(inqueue, outqueue)
-    end
-    t.join
-    outqueue.pop.should eql EOS
+  before(:each) do
+    @inqueue = Queue.new
+    @outqueue = Queue.new
   end
   
+  it "should put EOS on queue when no entries in input" do
+    extractor = W1913EntryExtractor.new('','', EOS)
+    run_extractor(extractor)
+    @outqueue.pop.should eql EOS
+  end
+  
+  it "should put one matched entry on the queue" do
+    entry = "<hw>A</hw>x"
+    input = "#{entry}#{EOS}"
+    @inqueue << input
+    pattern = '\<hw\>.*?\<\/hw\>'
+    
+    extractor = W1913EntryExtractor.new(pattern, pattern, EOS)
+    run_extractor(extractor)
+    @outqueue.pop.should eql entry
+    @outqueue.pop.should eql EOS
+  end
+
+  private
+  def run_extractor(extractor)
+    t = Thread.new do
+      extractor.extract(@inqueue, @outqueue)
+    end
+    t.join    
+  end
 end
